1f4200142c5ed0439337e260d4dbccfc117ab17b
sasszz <luchevreuil@gmail.com>
2023-05-03 12:17:07 -0700

    Added inline comments

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+// ##################################################################################################//
+/////////////////////////////////////// CHOPSTICKS GAME ALGORITHM //////////////////////////////////////
+// ##################################################################################################//
+
+// INITIALIZE VARIABLES //////////////////////////////////////////////////////////////////////////////
 let selfRight = 0,
   selfLeft = 1,
   oppRight = 1,
   oppLeft = 1;
 
 let selfTotalFingers = selfLeft + selfRight;
-console.log(`Self Left = ${selfLeft}, Self Right = ${selfRight}`);
-console.log(`Opp Left = ${oppLeft}, Opp Right = ${oppRight}`);
-console.log("Self Total Fingers = " + selfTotalFingers);
 let oppTotalFingers = oppLeft + oppRight;
 
+// ATTACK FUNCTION //////////////////////////////////////////////////////////////////////////////
 const attack = (oppHand, selfHand) => {
   console.log("Attack function called");
   oppHand = selfHand + oppHand;
+  if (oppHand >= 5) {
+    // Accounts for destroying opponents hand
+    oppHand = 0;
+  }
   return oppHand;
 };
 
+// SPLIT FUNCTION //////////////////////////////////////////////////////////////////////////////
 const split = () => {
   console.log("Split function called");
   if (selfTotalFingers == 4) {
+    // special case for 4 because we do not want a situation where a person can have 4 fingers on one hand
     selfRight == 2
       ? (selfRight = 1) & (selfLeft = 3)
       : (selfRight = 2) & (selfLeft = 2);
@@ -34,24 +43,35 @@ const split = () => {
   }
 };
 
-if (selfTotalFingers == 1 || selfTotalFingers == 7 || selfTotalFingers == 8) {
-  let returnValue = attack(selfLeft, oppLeft);
-  oppLeft = returnValue;
-  console.log("First Option, function calls ended");
-}
+// TURN FUNCTION //////////////////////////////////////////////////////////////////////////////
+const myTurn = () => {
+  console.log("Beginning of turn:");
+  console.log(`Self Left = ${selfLeft}, Self Right = ${selfRight}`);
+  console.log(`Opp Left = ${oppLeft}, Opp Right = ${oppRight}`);
+  console.log("Self Total Fingers = " + selfTotalFingers);
 
-if (
-  selfTotalFingers == 2 ||
-  selfTotalFingers == 3 ||
-  selfTotalFingers == 4 ||
-  selfTotalFingers == 5 ||
-  selfTotalFingers == 6
-) {
-  let returnValue = attack(selfLeft, oppLeft);
-  oppLeft = returnValue;
-  // split();
-  console.log("Second Option, function calls ended");
-}
+  if (selfTotalFingers == 1 || selfTotalFingers == 7 || selfTotalFingers == 8) {
+    // Cannot split on these combinations of fingers
+    let returnValue = attack(selfLeft, oppLeft);
+    oppLeft = returnValue;
+    console.log("First Option, function call ended");
+  }
 
-console.log(`Self Left = ${selfLeft}, Self Right = ${selfRight}`);
-console.log(`Opp Left = ${oppLeft}, Opp Right = ${oppRight}`);
+  if (
+    selfTotalFingers == 2 ||
+    selfTotalFingers == 3 ||
+    selfTotalFingers == 4 ||
+    selfTotalFingers == 5 ||
+    selfTotalFingers == 6
+  ) {
+    // User can split or attack on these combinations of fingers
+    let returnValue = attack(selfLeft, oppLeft);
+    oppLeft = returnValue;
+    // split(); // Commenting out split function for now
+    console.log("Second Option, function call ended");
+  }
+
+  console.log("End of turn:");
+  console.log(`Self Left = ${selfLeft}, Self Right = ${selfRight}`);
+  console.log(`Opp Left = ${oppLeft}, Opp Right = ${oppRight}`);
+};
